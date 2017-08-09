b3b8c0baae1305a95a09babcbb7fb23c1e668bea
chandra mouli <codechandra@gmail.com>
2017-08-09 02:42:11 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/', function (req, res) {
 app.get('/article', function (req, res) {
   res.send('Hi');
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
