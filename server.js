84e69e4537a72d9d96aa0880785c862ceb5f4cac
chandra mouli <codechandra@gmail.com>
2017-08-14 18:59:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
+app.get('/thankyou', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'thankyou.html'));
+});
 app.get('/article', function (req, res) {
   res.send('Hi');
 });
