3750f15d9776457df1721589cc4a36e4239708fa
chandra mouli <codechandra@gmail.com>
2017-09-01 15:21:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config={
     user:'codechandra',
     database:'http://db.imad.hasura-app.io',
     host:'5432',
-    password:process.env.DB_PASSWORD
+    password:process.ENV.DB_PASSWORD
 };
 var pool=new Pool(config);
 
