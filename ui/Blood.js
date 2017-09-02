6a608f18dd77596f2621e109775eb1d2940dff05
chandra mouli <codechandra@gmail.com>
2017-09-02 12:07:01 +0530

    [imad-console] Updates ui/Blood.js

--- ui/Blood.js
+++ ui/Blood.js
@@ -1,3 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var crypto = require('crypto');
+var app = express();
+var Pool=require('pg').Pool;
+
+app.use(morgan('combined'));
+var config={
+    user:'codechandra',
+    database:'codechandra',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+var pool=new Pool(config);
 
 function need()
 {
@@ -17,5 +33,14 @@ function search_donor()
 function search_recipent()
 {
     var recipentNumber=document.getElementById('number_recipent').value;
+    pool.query('INSERT into donor (mobile) VALUES (recipentNumber)',function(err,result){
+        if(err){
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send("success");
+       }
+    });
     
 }
