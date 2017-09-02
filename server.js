e17fad2b5ae89b78ba0611219fec3e70551d62b0
chandra mouli <codechandra@gmail.com>
2017-09-02 10:44:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/test-db',function(req,res){
    }) ;
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'evm.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'Blood.html'));
 });
 app.get('/thankyou.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'thankyou.html'));
