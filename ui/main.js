1f38e5436ad2f2b9ff17f9a372e891032e22b1a6
chandra mouli <codechandra@gmail.com>
2017-09-02 14:52:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,17 @@ function donor()
     var group=document.getElementById('donor_group');
     var opt = group.options[group.selectedIndex].text;
     var donor_name=document.getElementById('name_donor').value;
-    var s='/'+num+'$'+opt+'$'+donor_name;
+    var s='/donor'+num+'$'+opt+'$'+donor_name;
+   window.open(s);
+    
+}
+function recipentr()
+{
+    var num=document.getElementById('number_recipent').value;
+    var group=document.getElementById('recipent_group');
+    var opt = group.options[group.selectedIndex].text;
+    var donor_name=document.getElementById('name_recipent').value;
+    var s='/recipent'+num+'$'+opt+'$'+donor_name;
    window.open(s);
     
 }
