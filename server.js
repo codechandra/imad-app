7dc5f7ef8dbf5c29d8632f214488cc1a60d39e0d
chandra mouli <codechandra@gmail.com>
2017-09-02 15:18:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/recipent/:id',function(req,res){
        }
        else
        {
-           res.send(JSON.stringify(result.rows));
+           res.send(JSON.stringify(result.rows.donor_name));
        }
        
    }) ;
