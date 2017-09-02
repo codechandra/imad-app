263f0989093080c0877f4ec59dd5b245c251a487
chandra mouli <codechandra@gmail.com>
2017-09-02 13:41:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ function donor()
     var num=document.getElementById('number_recipent').value;
     var group=document.getElementById('h');
     var opt = group.options[group.selectedIndex];
-    window.alert(group.text);
+    window.alert(opt.text);
     
    // var s='/'+num;
 //    window.open(s);
