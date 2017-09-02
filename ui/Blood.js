a80299d54d4395ee72f263104638f2d3c52a678e
chandra mouli <codechandra@gmail.com>
2017-09-02 11:19:00 +0530

    [imad-console] Updates ui/Blood.js

--- ui/Blood.js
+++ ui/Blood.js
@@ -1,1 +1,11 @@
 
+function need()
+{
+  document.getElementById('needd').style.display='block';
+    document.getElementById('donatee').style.display='none';
+}
+function donate()
+{
+  document.getElementById('donatee').style.display='block';
+  document.getElementById('needd').style.display='none';
+}
