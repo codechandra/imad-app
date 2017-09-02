05d1acacd3319f8bcc1ab1c6a5d2da155ed10188
chandra mouli <codechandra@gmail.com>
2017-09-02 12:49:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,65 +27,24 @@ app.get('/test-db',function(req,res){
        }
    }) ;
 });
-app.get('/', function (req, res) {
-  res.sendFile('Blood.html');
-  
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function need()
-{
-  document.getElementById('needd').style.display='block';
-    document.getElementById('donatee').style.display='none';
-}
-function donate()
-{
-  document.getElementById('donatee').style.display='block';
-  document.getElementById('needd').style.display='none';
-}
-
-function search_donor()
-{
-    window.alert(document.getElementById('number_recipent').value);
-}
-function search_recipent()
-{
-    var recipentNumber=document.getElementById('number_recipent').value;
-    
-}
-app.get('/thankyou.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'thankyou.html'));
-});
-app.get('/signup.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'signup.html'));
-});
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512, 'sha512');
     return hashed.toString('hex');    
     
 }
-app.get('/vote.html',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'vote.html'));
-});
-app.get('/Blood.html', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'Blood.html'));
-});
 
 
 app.get('/hash/:input', function (req, res) {
     var hashedString=hash(req.params.input,'this-is-some-random-string');
   res.send(hashedString);
 });
-app.get('/forgot.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'forgot.html'));
-});
-app.get('/article', function (req, res) {
-  res.send('Hi');
-});
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
