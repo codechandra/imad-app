5621908237a59f9785f09c73521e4bca5c8728f9
chandra mouli <codechandra@gmail.com>
2017-09-03 22:44:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,23 +18,18 @@ function donor()
     var opt = group.options[group.selectedIndex].text;
     var donor_name=document.getElementById('name_donor').value;
     var s='/donor/'+num+'$'+opt+'$'+donor_name;
-    var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        };
-
-        anHttpRequest.open( "GET", s, true );            
-        anHttpRequest.send( null );
-    };
+    
+   var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", s, true);
+  xhttp.send();
 }
-var client = new HttpClient();
-client.get('http://some/thing?with=arguments', function(response) {
-    // do something with response
-});
-  
+   
     
 }
 function recipent()
@@ -44,23 +39,7 @@ function recipent()
     var opt = group.options[group.selectedIndex].text;
     var recipent_name=document.getElementById('name_recipent').value;
     var s='/recipent/'+num+'$'+opt+'$'+recipent_name;
-    var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        };
-
-        anHttpRequest.open( "GET", s, true );            
-        anHttpRequest.send( null );
-    };
-}
-var client = new HttpClient();
-client.get('http://some/thing?with=arguments', function(response) {
-    // do something with response
-});
-   
+   var d=window.open(s);
    
     
 }
