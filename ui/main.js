521baf1a28e8c98146c9e83fda5ee039d20a18d5
chandra mouli <codechandra@gmail.com>
2017-09-03 22:37:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,23 @@ function donor()
     var opt = group.options[group.selectedIndex].text;
     var donor_name=document.getElementById('name_donor').value;
     var s='/donor/'+num+'$'+opt+'$'+donor_name;
-   var d=window.open(s);
+    var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        };
+
+        anHttpRequest.open( "GET", s, true );            
+        anHttpRequest.send( null );
+    };
+}
+var client = new HttpClient();
+client.get('http://some/thing?with=arguments', function(response) {
+    // do something with response
+});
+  
     
 }
 function recipent()
@@ -28,7 +44,23 @@ function recipent()
     var opt = group.options[group.selectedIndex].text;
     var recipent_name=document.getElementById('name_recipent').value;
     var s='/recipent/'+num+'$'+opt+'$'+recipent_name;
-   var d=window.open(s);
+    var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        };
+
+        anHttpRequest.open( "GET", s, true );            
+        anHttpRequest.send( null );
+    };
+}
+var client = new HttpClient();
+client.get('http://some/thing?with=arguments', function(response) {
+    // do something with response
+});
+   
    
     
 }
