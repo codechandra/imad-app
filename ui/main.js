5454d160c564586bd292201844ca05f4495f7671
chandra mouli <codechandra@gmail.com>
2017-09-04 14:23:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,8 +46,6 @@ function recipent()
     if (this.readyState == 4 && this.status == 200) {
     document.getElementById("main").style.display = 'none';
     var d=JSON.stringify(this.responseText);
-    var r=new object();
-    r=d;
       document.getElementById("demo").innerHTML = typeof(r);
      // document.getElementById("demo").innerHTML = this.responseText;
     }
