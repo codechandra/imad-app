8f0ea7938d99319e2d891ab7147b460ae9b88f72
chandra mouli <codechandra@gmail.com>
2017-09-04 18:42:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,7 +45,7 @@ function recipent()
     if (this.readyState == 4 && this.status == 200) {
         d=JSON.parse(this.responseText);
     document.getElementById("main").style.display = 'none';
-      document.getElementById("demo").innerHTML = d[1][".donor_mobile"];
+      document.getElementById("demo").innerHTML = d[1]["donor_mobile"];
     }
   };
   xhttp.open("GET", s, true);
