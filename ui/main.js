a4e4e056f71d0606957f75b42d5f61830b81decc
chandra mouli <codechandra@gmail.com>
2017-09-04 14:14:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ function recipent()
     var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        d=JSON.parse(this.responseText);
+        d=JSON.stringify(this.responseText);
     document.getElementById("main").style.display = 'none';
       document.getElementById("demo").innerHTML = d;
      // document.getElementById("demo").innerHTML = this.responseText;
