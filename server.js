dcd03d63b4b508de19e7a6637213155fcf265209
chandra mouli <codechandra@gmail.com>
2017-09-05 22:03:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/test-db',function(req,res){
        }
    }) ;
 });
-app.get('/hai', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
