ac4fe3e6a1d20d1014a0ce7bdea0cc6f0470632a
chandra mouli <codechandra@gmail.com>
2017-09-05 19:12:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,22 +43,9 @@ function recipent()
     var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        d=JSON.parse(this.responseText);
+       // d=JSON.parse(this.responseText);
     document.getElementById("main").style.display = 'none';
-   // document.getElementById("demo").innerHTML = d;
-    var i=0,k=0,j=2;
-    for(i=0;i<d.length;i++){
-        alert(d[i]["donor_mobile"]);
-    }
-    for(i=0;i<d.length;i++)
-    {    k=(2*k)+1;
-         document.getElementById(k).innerHTML = d[i]["donor_name"];
-         alert(k);
-         document.getElementById(j).innerHTML = d[i]["donor_mobile"];
-         j=(2+j);
-        }
-        // alert(d[i]["donor_name"]);
-        // alert(d[i]["donor_mobile"]);
+    document.getElementById("demo").innerHTML = this.responseText;
     
      
     }
