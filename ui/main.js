0c359da365ba0baa4154b3c6b2e90bb3a82bc798
chandra mouli <codechandra@gmail.com>
2017-10-28 19:56:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ function login_fun()
 {
     document.getElementById('login').style.display="block";
 }
+function register_fun()
+{
+     document.getElementById('login').style.display="none";
+      document.getElementById('register').style.display="block";
+}
