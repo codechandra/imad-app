bf1459b1703e2193ceace01f5156da9179d58e91
chandra mouli <codechandra@gmail.com>
2017-11-01 18:31:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -129,6 +129,7 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
        //document.getElementById("demo").innerHTML = xhttp.responseText;
+       alert(xhttp.responseText);
        
     }
 };
