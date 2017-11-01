3744d39f39a43a9a914517d3f2d045f97927892e
chandra mouli <codechandra@gmail.com>
2017-11-01 20:03:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,11 +42,7 @@ app.get('/hello/:id',function(req,res){
     var one=(m[0]);
     var two=(m[1]);
     var three=m[2];
-    var four=parseInt(m[3]);
-    var five=m[4];
-    var six=m[5];
-    var seven=m[6];
-    var eight=m[7];
+    var four=(m[3]);
     pool.query('INSERT INTO seller(sname,semail,spassword,smobile) VALUES ($1,$2,$3,$4)',[one,two,three,four],function(err,result){
        if(err){
            res.status(500).send(err.toString());
