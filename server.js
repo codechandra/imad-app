23bc3d6f1ec815fcac62a5f1f058daeb6fe4712c
chandra mouli <codechandra@gmail.com>
2017-11-03 12:23:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/hai1/:id',function(req,res){
     var three=m[2];
     if(m[2]==1)
     {
-         pool.query('SELECT *FROM customer where (cmail=one&&cpassword=two)',function(err,result){
+         pool.query('SELECT *FROM customer where (cmail=one AND cpassword=two)',function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }
