0ecd41ddfcdfe774eef990e331a7d98e66791643
chandra mouli <codechandra@gmail.com>
2017-11-07 18:27:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -205,7 +205,7 @@ function propertyregister_fun()
      var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      alert(xhttp.responseText);
+
     }
 };
 xhttp.open("GET", s, true);
